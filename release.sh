9df62521dc210248c97e7793b74d9cc7578fc80d
Gevik Babakhani <gevik@blendjs.com>
2020-11-16 21:23:55 +0100

    fix: Mon Nov 16 21:23:55 CET 2020

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO_STATE=`git diff-index --quiet HEAD`
+LERNA=./node_modules/.bin/lerna
 BRANCH_NAME=`git branch |grep \* | cut -d " " -f2`
 DIST_TAG=beta
 RELEASE_BRANCH="release-`date +%s`"
@@ -7,5 +7,7 @@ if [ "${BRANCH_NAME}" = "master" ]; then
     DIST_TAG=latest
 fi;
 
-echo "$REPO_STATE"
-#git checkout -b ${RELEASE_BRANCH}
+git checkout -b ${RELEASE_BRANCH}
+if [ "$DIST_TAG" === "beta"]; then
+    $LERNA version --conventional-commits --conventional-prerelease --no-changelog --no-git-tag-version --no-push --preid ${DIST_TAG}
+fi;
